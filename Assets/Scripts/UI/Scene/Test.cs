5f06c136858087fb6ad664551a2597bb5f0ee7ff
starkshn <starkshn97@gmail.com>
2021-09-26 01:20:00 +0900

    Test Commit in window
    Test

---
+++ Assets/Scripts/UI/Scene/Test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test
+{
+
+    public void Say()
+    {
+        Debug.Log("Say Hello!");
+
+    }
+}
