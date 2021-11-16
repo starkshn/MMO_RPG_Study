3f7b949c93d5a5b4840684f37ab19de5b3561e7a
starkshn <starkshn97@gmail.com>
2021-11-17 00:32:19 +0900

    Coroutine #2 1-1
    1. Coroutine 정리
    2. 어디에 쓰는지 설명

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -9,6 +9,12 @@ public class GameScene : BaseScene
         public int id = 0;
     }
 
+    // Coroutine을 정리하자면은
+    // 1. 함수의 상태를 저장or복원이 가능!
+    // 그러면 언제 쓰는것인가?
+        // -> 엄청 오래 걸리는 작업을 끊거나
+        // -> 원하는 타이밍에 함수를 잠시 Stop/복원하는 경우   이런 경우에 사용하면 좋다.
+    // 2. return -> 우리가 원하는 타입으로 가능 (class도 가능)
     class CoroutineTest : IEnumerable
     {
         public IEnumerator GetEnumerator()
@@ -18,6 +24,15 @@ public class GameScene : BaseScene
                 if(i % 10000 == 0)
                     yield return null;
             }
+        }
+
+        void GenerateItem()
+        {
+            // 아이템을 만들어준다.
+            // DB저장
+
+            // 멈춤
+            // 로직
         }
     } 
 
@@ -31,11 +46,11 @@ public class GameScene : BaseScene
 
         CoroutineTest test = new CoroutineTest();
 
-        foreach(System.Object t in test)
-        {
-            Test value = (Test)t;
-            Debug.Log(value.id);
-        }
+        //foreach(System.Object t in test)
+        //{
+        //    Test value = (Test)t;
+        //    Debug.Log(value.id);
+        //}
     }
 
 
