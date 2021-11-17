79cd9ae354ee025b4846159fc767bed731f58898
starkshn <starkshn97@gmail.com>
2021-11-17 23:49:08 +0900

    Data Manager#1 1-1
    1. Data의 개념
    2. Data Manager가 필요한 이유

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
 
     protected override void init()
     { 
@@ -14,33 +13,7 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        co = StartCoroutine("CoExplodeAfter4Seconds", 4.0f);
-
-        StartCoroutine("CoStopExplod", 2.0f);
-         
-
     }
-
-    IEnumerator CoStopExplod(float seconds)
-    {
-        Debug.Log("Stop Enter!");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Execute!!!!");
-        if(co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-     
-    IEnumerator CoExplodeAfter4Seconds(float seconds)
-    {
-        Debug.Log("Explode Enter!");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Execute!!!!");
-        co = null;
-    }
-
 
     public override void Clear()
     {
