a64825bcd7e7ac226ded74059a184035d7119a7f
starkshn <starkshn97@gmail.com>
2021-11-17 21:12:48 +0900

    Coroutine #2 1-2
    1. C++ Coroutine지원 X
    2. Csharp 지원 O
    3. yield return -> (Object, class etc..) 다가능
    4. StartCoroutine, StopCoroutine -> 컨텐츠 제작에 활용 굿굿 짱짱

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,37 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    class Test
-    {
-        public int id = 0;
-    }
-
-    // Coroutine을 정리하자면은
-    // 1. 함수의 상태를 저장or복원이 가능!
-    // 그러면 언제 쓰는것인가?
-        // -> 엄청 오래 걸리는 작업을 끊거나
-        // -> 원하는 타이밍에 함수를 잠시 Stop/복원하는 경우   이런 경우에 사용하면 좋다.
-    // 2. return -> 우리가 원하는 타입으로 가능 (class도 가능)
-    class CoroutineTest : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
-        {
-            for(int i = 0; i< 1000000; i++)
-            {
-                if(i % 10000 == 0)
-                    yield return null;
-            }
-        }
-
-        void GenerateItem()
-        {
-            // 아이템을 만들어준다.
-            // DB저장
-
-            // 멈춤
-            // 로직
-        }
-    } 
+    Coroutine co;
 
     protected override void init()
     { 
@@ -44,13 +14,31 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        CoroutineTest test = new CoroutineTest();
+        co = StartCoroutine("CoExplodeAfter4Seconds", 4.0f);
 
-        //foreach(System.Object t in test)
-        //{
-        //    Test value = (Test)t;
-        //    Debug.Log(value.id);
-        //}
+        StartCoroutine("CoStopExplod", 2.0f);
+         
+
+    }
+
+    IEnumerator CoStopExplod(float seconds)
+    {
+        Debug.Log("Stop Enter!");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Execute!!!!");
+        if(co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+     
+    IEnumerator CoExplodeAfter4Seconds(float seconds)
+    {
+        Debug.Log("Explode Enter!");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Execute!!!!");
+        co = null;
     }
 
 
