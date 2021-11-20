c830f4948156943649881774c7521537ace34acf
starkshn <starkshn97@gmail.com>
2021-11-20 16:38:28 +0900

    Data Manager#1 1-1
    1.Serialize 개념
    2. Json, XML 사용
    3. DeSerialize 하는방법, Sereialize하는 방법

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -10,19 +10,23 @@ public class Managers : MonoBehaviour
     // 외부에서 얘를 사용하고싶을때는
     static Managers Instance { get { init(); return s_instance; } } // 유일한 매니저를 갖고온다 
 
+
+    DataManager _data = new DataManager();
     InputManager _input = new InputManager();
     PoolManager _pool = new PoolManager();
     ResourceManager _resource = new ResourceManager();
     SoundManager _sound = new SoundManager();
     SceneManagerEx _scene = new SceneManagerEx();
-    UIManager ui = new UIManager();
+    UIManager _ui = new UIManager();
 
+
+    public static DataManager Data { get { return Instance._data;  } }
     public static InputManager Input { get { return Instance._input;  } }
     public static PoolManager Pool { get { return Instance._pool; } }
     public static ResourceManager Resource {  get { return Instance._resource;  } }
     public static SceneManagerEx Scene { get { return Instance._scene; } } // SceneManager는 UI위에 위치하니까 여기다가 넣어주자
     public static SoundManager Sound { get { return Instance._sound; } }
-    public static UIManager UI { get { return Instance.ui; } }
+    public static UIManager UI { get { return Instance._ui; } }
 
     void Start()
     {
@@ -53,6 +57,8 @@ public class Managers : MonoBehaviour
             // 여기서 Instance 호출하면 안된다! 주의 해야함 왜냐하면 static Managers Instance { get { Init(); return s_instance; } }
             // 에서 return으로 Init()을 호출하고있기때문에 무한루프에 빠질 수 있다.
 
+
+            s_instance._data.init();
             s_instance._sound.init();
 
             s_instance._pool.init(); // pool에 init하는 부분이 있었으니까 챙겨주자
