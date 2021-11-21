3343c047f742def279b7ed3c3f0216f9e37b100e
starkshn <starkshn97@gmail.com>
2021-11-21 14:06:36 +0900

    Data Manager#2
    1. Reflection의 중요성과 효율성 (굿굿)
    2. Json파일 형식 맞춰주고, 변수명 맞춰주기, 자동 파싱
    3. id를 통해서 Dict형태로 데이터 빨리 찾기 구현
    4. 하드코딩이 많으니까 전부다 수정작업(진행중)

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,18 +13,38 @@ public class Stat
 } 
 
 [Serializable]
-public class StatData
+public class StatData : ILoader<int, Stat>
 {
     public List<Stat> stats = new List<Stat>();
+
+    public Dictionary<int, Stat> MakeDict()
+    {
+        Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
+
+        foreach (Stat stat in stats)
+            dict.Add(stat.level, stat);
+
+        return dict;
+    }
+}
+
+public interface ILoader<Key, Value>
+{
+    Dictionary<Key, Value> MakeDict();
 }
 
 public class DataManager
 {
 
+    public Dictionary<int, Stat> StatDict { get; private set; } = new Dictionary<int, Stat>();
+    
+
     public void init()
     {
         TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/StatData");
         StatData data = JsonUtility.FromJson<StatData>(textAsset.text);
+
+
     }
 
 }
